e04dda1d5f79731e7c17e349a866f38fce835962
popehiflo <poolpetterhijuel@hotmail.com>
2022-05-19 14:24:06 -0500

    feat: install express, server up

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("I'm the entry point");
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json('Hi, response to GET /');
+});
+
+app.listen(8080, () => {
+  console.log("I'm here ... I'm server!");
+});
+
+

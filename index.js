5f8375646915cde8bc65458ad497ef5effded5c9
popehiflo <poolpetterhijuel@hotmail.com>
2022-05-22 22:40:49 -0500

    feat: config connection to MongoDB

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 const express = require('express');
 
 const configExpress = require('./config/express');
+const connectionDB = require('./config/database');
 const routes = require('./routes');
 
 const app = express();
 
 configExpress(app);
-
+connectionDB();
 routes(app);
 
 const port = process.env.PORT || 8080;

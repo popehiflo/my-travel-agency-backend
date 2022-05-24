874f7476a2b9b918ab1732057711a571ca9777ea
popehiflo <poolpetterhijuel@hotmail.com>
2022-05-24 12:19:59 -0500

    feat: verificaciones de Token y role

--- auth/auth.local.token.js
+++ auth/auth.local.token.js
@@ -1,7 +1,8 @@
 const JWT = require('jsonwebtoken');
 
+// Verifica el token (x-token)
 const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.x-token;
+  const authHeader = req.headers['x-token'];
   if (authHeader) {
     const token = authHeader.split(" ")[1];
     JWT.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
@@ -14,6 +15,31 @@ const verifyToken = (req, res, next) => {
   }
 };
 
+// Verifica el token y que sea el mismo usuario o el Admin
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    console.log(req.user);
+    if (req.user.id === req.params.id || req.user.role === 'ADMIN') {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
+
+// Verifica el token y que sea admin
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.role === 'ADMIN') {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
+
 module.exports = {
   verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin
 };

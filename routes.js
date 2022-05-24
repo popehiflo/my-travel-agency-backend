452f8a0807cabc021aa43024ed452dce23e1c11f
popehiflo <poolpetterhijuel@hotmail.com>
2022-05-24 00:06:22 -0500

    feat: defined API routes

--- routes.js
+++ routes.js
@@ -2,10 +2,16 @@
  * Main application routes
  */
 const user = require('./api/user');
+const tour = require('./api/tour');
+const order = require('./api/order');
+const backpack = require('./api/backpack');
 
 function routes(app) {
   // API Routes
   app.use('/api/users', user);
+  app.use('/api/tours', tour);
+  app.use('/api/orders', order);
+  app.use('/api/backpacks', backpack);
 }
 
 module.exports = routes;
